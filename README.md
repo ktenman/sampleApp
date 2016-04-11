cf16ee7f4344226cb9de1289838e6087e2ffb0dc
ktenman <ktenman@users.noreply.github.com>
2016-04-11 18:13:24 +0300

    week 2

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
-# sampleApp
-node.js Hello World
+```JS
+//config.js
+module.exports = {
+    port: 3000,
+    db: 'mongodb://user:password@host:port/database'
+};
+```
--- index.js
+++ index.js
@@ -1,42 +1,31 @@
 var express = require('express');
-var app = express();
 var morgan = require('morgan');
 var bodyParser = require('body-parser');
-var mongoose = require('mongoose');
 var config = require('./config');
-
-// and after the app
-
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(bodyParser.json());
-
-app.use(morgan('dev'))
+var mongoose = require('mongoose');
 
 mongoose.connect(config.db);
 
-var topics = require('./routes/topics');
+var app = express();
 
-// after app declaration
+app.use(morgan('dev'));
+
+// retrieve POST data
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+//ROUTES
+var topics = require('./routes/topics');
 app.use('/api/topics', topics);
 
-
-app.get('/api/', function(req, res){
-    console.log('Somebody visited /api');
-    res.json({"success": "Tere tulemast!"});
-});
-
-app.listen(3000, function(){
-    console.log('App started on port 3000!');
-});
-
+//404
 app.use(function(req, res, next) {
     var err = new Error('Resource not found');
     err.status = 404;
     next(err);
 });
-
 app.use(function(err, req, res, next) {
-    console.error("["+(err.status || 500)+"] "+(new Date).toString()+" "+req.url +' '+ err);
+    console.error("["+(err.status || 500)+"] "+(new Date()).toString()+" "+req.url +' '+ err);
     var message = err.status == 404 ? err.message : "Unknown error";
     res.status(err.status || 500).json({
         status: err.status || 500,
@@ -44,10 +33,12 @@ app.use(function(err, req, res, next) {
     });
 });
 
-//kostik();
-
 process.on('uncaughtException', function (err) {
-    console.error((new Date).toString() + ' uncaughtException:', err.message)
-    console.error(err.stack)
-    process.exit(1)
+    console.error((new Date()).toString() + ' uncaughtException:', err.message);
+    console.error(err.stack);
+    process.exit(1);
+});
+
+app.listen(3000, function(){
+    console.log('App started on port 3000!');
 });
